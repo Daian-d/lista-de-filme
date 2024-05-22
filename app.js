f840b60fba988e1f40b72d8b63f950c80a8ce567
Daian-d <124202818+Daian-d@users.noreply.github.com>
2024-05-22 07:38:08 -0300

    Foi realizado alterações no codigo e atualização da lsiat de filmese clink de acesso

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
+  //Lista de filmes
+   var listaFilmes = [
+    "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/11/f_1pwghxkaax39m__9bfxp2-760x760.jpeg", //Aquqmqn
+    "https://upload.wikimedia.org/wikipedia/pt/thumb/6/69/The_Avengers_Cartaz.jpg/250px-The_Avengers_Cartaz.jpg", //Os Vingadores
+    "https://upload.wikimedia.org/wikipedia/pt/d/da/Minha_M%C3%A3e_%C3%A9_uma_Pe%C3%A7a.jpg", //Minha mãeé uma peça
+    "https://br.web.img3.acsta.net/pictures/18/03/01/20/26/0577579.jpg", //Um lugar silencioso
+    "https://br.web.img3.acsta.net/medias/nmedia/18/90/57/96/20121842.jpg",  //Batman
+    "https://br.web.img2.acsta.net/pictures/19/07/05/17/30/5167951.jpg", //Homem Aranha longe de casa
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKH0yi10DPgDYbgGLDi5LeyQM8a8epz6ztHQ&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo5Zh9HAjLtTyORUI0AdctkhzBSny2C4-gOA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_bqu36FpZlMelvb3tlwe5CDK0kZTGSXy3zg&s"
 
-var listaFilmes = ["https://uploads.jovemnerd.com.br/wp-content/uploads/2023/11/f_1pwghxkaax39m__9bfxp2-760x760.jpeg","https://upload.wikimedia.org/wikipedia/pt/thumb/6/69/The_Avengers_Cartaz.jpg/250px-The_Avengers_Cartaz.jpg","https://upload.wikimedia.org/wikipedia/pt/d/da/Minha_M%C3%A3e_%C3%A9_uma_Pe%C3%A7a.jpg","https://br.web.img3.acsta.net/pictures/18/03/01/20/26/0577579.jpg","https://br.web.img3.acsta.net/medias/nmedia/18/90/57/96/20121842.jpg","https://i.ibb.co/FxHf1rm/img-4718-854x540.jpg","https://br.web.img2.acsta.net/pictures/19/07/05/17/30/5167951.jpg","https://upload.wikimedia.org/wikipedia/pt/9/9f/Sonic_the_Hedgehog_2019.jpg","https://br.web.img3.acsta.net/pictures/17/06/29/14/58/351355.jpg","https://pipocanamadrugada.com.br/site/wp-content/uploads/2016/07/velozes-e-furiosos-7_pipoca-na-madrugada-1.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqpIWaMFXvpWWM7vYJuHGpUsQrD3HcvQ9xVA&usqp=CAU","https://upload.wikimedia.org/wikipedia/pt/4/41/Fast_%26_Furious_9_The_Fast_Saga.jpg","https://br.web.img2.acsta.net/pictures/23/05/16/22/53/0156803.jpg"]
-var nomeFilmes = ["Aquaman","Os vingadores","Minha Mãe é uma Peça","Um lugar Silencioso","Batman Cavaleiro das Trevas","Titãs","Homem-Aranha Longe de Casa","Sonic","Dupla Explosiva","Velozes e Furiosos 7","Velozes e Furiosos 8","Velozes e Furiosos 9","Veloses e Furioso 10"]
-
-for (var i = 0; i < listaFilmes.length; i++) {
-   document.write("<div style='display: inline-block; margin: 10px;'>");
- //document.write("<div>");
- 
-  document.write("<img src=" + listaFilmes[i] + ">");
-   document.write("<p style='color: red; font-size:13px'>" + nomeFilmes[i] + "</p>");
-  document.write("</div>");
-}
-
+  ]; //Velozes e furiosos 10
+  
+  //lista do nome do filme  
+  var nomeFilmes = ["Aquaman",
+   "Os vingadores", 
+   "Minha Mãe é uma Peça", 
+   "Um lugar Silencioso", 
+   "Batman Cavaleiro das Trevas", 
+   "Homem-Aranha Longe de Casa",
+   "Donzela", 
+   "Oppenheimer",
+   "Dungeons & Dragons"
+  
+  ];
+  
+  // Lista de URLs dos trailers
+  var listaTrailers = [
+      "https://www.youtube.com/watch?v=yJnzCBt5j1M", // Trailer de Aquaman
+      "https://www.youtube.com/watch?v=KeNEGtsCWEk", // Trailer de Os Vingadores
+      "https://www.youtube.com/watch?v=HrST-4WLlbA", // Trailer de Minha Mãe é uma Peça
+      "https://www.youtube.com/watch?v=ENocUCWoGHA", // Trailer de Um lugar silenciso
+      "https://www.youtube.com/watch?v=zqfz04yCTnE", // Trailer Batman o cavaleiro das trevas
+      "https://www.youtube.com/watch?v=L8qtbebt-y0", // Trailer do Hommem-Aranha longe de casa
+      "https://www.youtube.com/watch?v=WvFHERDoBVg", // Tailer da Donzela
+      "https://www.youtube.com/watch?v=F3OxA9Cz17A", // Tailer do Oppenheimer
+      "https://www.youtube.com/watch?v=PtewyAANr34"  //Trailer do Dungeons & Dragons
+    ]; 
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+      document.write("<div style='display: inline-block; margin: 10px;'>");
+      document.write("<a href='" + listaTrailers[i] + "' target='_blank'>"); // Define o link do trailer como o link de destino do <a>
+      document.write("<img src=" + listaFilmes[i] + ">");
+      document.write("</a>"); // Fecha a tag <a>  
+      document.write("<p style='color:red;font-size:15px'>" + nomeFilmes[i] + "</p>");
+      document.write("</div>");
+  }
+  
--- js/app.js
+++ js/app.js
@@ -1,8 +1,44 @@
-var listaSeries = ["https://infinitasvidas.files.wordpress.com/2020/06/peaky-blinders.png?w=640","https://upload.wikimedia.org/wikipedia/pt/2/25/Suits_temporada_7.jpg","https://portal.megabrasil.com.br/imagens/20200811_5f32a9012dc4e.jpg","https://t.ctcdn.com.br/YbDUBMmb1gDALkZFBg7lyAhxtVQ=/320x180/smart/i618290.jpeg","https://www.cdpoint.com.br/Imagens_main/789/851/297/7898512979500.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn2RVPlFxOiW7OO8btD8uW-DCua88g119v7GVdeel4Ux_ANY7O9IIuvW6Fe4dALGqrxuQ&usqp=CAU","https://pm1.aminoapps.com/6497/2a86ad8755fecb5b26054998dcfb20f7461a869e_hq.jpg","https://br.web.img2.acsta.net/pictures/23/09/14/22/21/5601463.png","https://poltronanerd.com.br/wp-content/uploads/2019/01/poltrona-Sex-education-poster.jpg","https://www.ecranlarge.com/media/cache/160x213/uploads/image/001/487/yrvucoxcwsxmia2ewem15adyjmn-295.jpg","https://img.elo7.com.br/product/zoom/4AA1F61/quadro-decorativo-serie-the-witcher-serie.jpg"]
-var nomeSeries = ["Peaky Blinders","Suits","Lucifer","Casa do Dreagão","Lobo Adolescente","Brighton","Grimm","One Piece","Sex Education","Vaganarok","The Witcher"]
-for(var i = 0; i < listaSeries.length; i++){
+//Lista de URLs da imagem das séries
+var listaSeries = [
+    "https://infinitasvidas.files.wordpress.com/2020/06/peaky-blinders.png?w=640", 
+    "https://upload.wikimedia.org/wikipedia/pt/2/25/Suits_temporada_7.jpg", 
+    "https://portal.megabrasil.com.br/imagens/20200811_5f32a9012dc4e.jpg", 
+    "https://t.ctcdn.com.br/YbDUBMmb1gDALkZFBg7lyAhxtVQ=/320x180/smart/i618290.jpeg", 
+    "https://br.web.img3.acsta.net/pictures/15/02/11/14/18/481673.jpg", 
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn2RVPlFxOiW7OO8btD8uW-DCua88g119v7GVdeel4Ux_ANY7O9IIuvW6Fe4dALGqrxuQ&usqp=CAU", 
+    "https://pm1.aminoapps.com/6497/2a86ad8755fecb5b26054998dcfb20f7461a869e_hq.jpg", 
+    "https://br.web.img2.acsta.net/pictures/23/09/14/22/21/5601463.png", 
+    "https://poltronanerd.com.br/wp-content/uploads/2019/01/poltrona-Sex-education-poster.jpg", 
+    "https://www.ecranlarge.com/media/cache/160x213/uploads/image/001/487/yrvucoxcwsxmia2ewem15adyjmn-295.jpg", 
+    "https://img.elo7.com.br/product/zoom/4AA1F61/quadro-decorativo-serie-the-witcher-serie.jpg",
+    "https://i.ibb.co/FxHf1rm/img-4718-854x540.jpg"];
+
+//Lista do nomes das séries
+var nomeSeries = ["Peaky Blinders","Suits","Lucifer","Casa do Dragão", "Lobo Adolescente", "Bridgerton", "Grimm", "One Piece", "Sex Education", "Vaganarok", "The Witcher","Titans"];
+
+// Lista de URLs dos trailers
+var listaTrailers = [
+    "https://www.youtube.com/watch?v=K-RoN7MA5V8", // Trailer de Peaky Blinders
+    "https://www.youtube.com/watch?v=B1OL4ZQf70c", // Trailer de Suits
+    "https://www.youtube.com/watch?v=wYljG3jiv-s", // Trailler de Lucifer
+    "https://www.youtube.com/watch?v=6ZdBiDGMMGI", // Trailerr da Casa do Draão
+    "https://www.youtube.com/watch?v=TLqKaFrjjNk", // Trailer de Teen Wolf
+    "https://www.youtube.com/watch?v=pyi8QAlHR8k", // Trailer de  Bridgerton
+    "https://www.youtube.com/watch?v=iVJ0FLNd2uQ", // Trailer de Grimm
+    "https://www.youtube.com/watch?v=YC8SLpnxsL4", //Trailler de Once Piece
+    "https://www.youtube.com/watch?v=nGe-uZEQh3g", // Trailer de Sex Education
+    "https://www.youtube.com/watch?v=DsZNcQSGRr8", // Trailer de Raganorck
+    "https://www.youtube.com/watch?v=tjujvMkqWe4", //Trailer de The Witcher 
+    "https://www.youtube.com/watch?v=iH44Z0QwrWQ"];//Trailer de Titans
+
+
+for (var i = 0; i < listaSeries.length; i++) {
     document.write("<div style='display: inline-block; margin: 10px;'>");
+    document.write("<a href='" + listaTrailers[i] + "' target='_blank'>"); // Define o link do trailer como o link de destino do <a>
     document.write("<img src=" + listaSeries[i] + ">");
-    document.write("<p style='color:red;font-size:13px'>" + nomeSeries[i] + "</p>");
+    document.write("</a>"); // Fecha a tag <a>  
+    document.write("<p style='color:red;font-size:15px'>" + nomeSeries[i] + "</p>");
     document.write("</div>");
 }
+
+
